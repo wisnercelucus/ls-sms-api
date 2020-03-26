8b55fa3871a2084effc70eb8e91cfddd26946466
wisnercelucus <wisnercelicus@gmail.com>
2020-03-26 10:58:30 -0400

    Create UserProfileserializer and UserProfileViewSet

--- src/ls_sms_api/userprofile/views.py
+++ src/ls_sms_api/userprofile/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+
+from . import serializers
+from . import models
+from rest_framework import status
 
 # Create your views here.
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
