9ff8bce77561641b6f7ba9b4e99378efbb8c0ef5
wisnercelucus <wisnercelicus@gmail.com>
2020-03-26 16:04:46 -0400

    Add login viewset

--- src/ls_sms_api/userprofile/urls.py
+++ src/ls_sms_api/userprofile/urls.py
@@ -5,7 +5,7 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 
 router.register('profile', views.UserProfileViewSet)
-
+router.register('login', views.LoginViewSet, basename='login')
 urlpatterns = [
     path('', include(router.urls))
 ]
--- src/ls_sms_api/userprofile/views.py
+++ src/ls_sms_api/userprofile/views.py
@@ -4,6 +4,10 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+
+
 
 from . import serializers
 from . import models
@@ -18,3 +22,11 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+class LoginViewSet(viewsets.ViewSet):
+
+    serializer_class = AuthTokenSerializer
+
+    def create(self, request):
+        """Create authentification token for the LoginViewSet"""
+        return ObtainAuthToken.post(self,request)
