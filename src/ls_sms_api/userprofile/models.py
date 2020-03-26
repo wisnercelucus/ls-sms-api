0f37a8ae8d75de426dd4364ed9e58411149a980a
wisnercelucus <wisnercelicus@gmail.com>
2020-03-25 21:54:07 -0400

    Commit changes including enabling django admin

--- src/ls_sms_api/userprofile/models.py
+++ src/ls_sms_api/userprofile/models.py
@@ -6,21 +6,21 @@ from django.contrib.auth.models import BaseUserManager
 
 class UserProfileManager(BaseUserManager):
 
-    def creat_user(self, email, first_name, last_name, password=None):
+    def creat_user(self, email, name, password=None):
         """Create a new user profile object."""
         if not email:
             raise ValueError("A user must have a valid email address")
 
         email = self.normalize_email(email)
-        user = self.model(email=email, first_name=first_name, last_name=last_name)
+        user = self.model(email=email, name=name)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
 
-    def create_superuser(self, email, first_name, last_name, password):
-        user = self.creat_user(email, first_name, last_name, password)
+    def create_superuser(self, email, name, password):
+        user = self.creat_user(email, name, password)
 
         user.is_superuser = True
         user.is_staff = True
@@ -35,25 +35,24 @@ class UserProfileManager(BaseUserManager):
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """This class represent represents a user profile inside of the system."""
     email = models.EmailField(max_length=255, unique=True)
-    first_name = models.CharField(max_length=255)
-    last_name_name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['fisrt_name', 'last_name']
+    REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
         """Use to get a User's fullname."""
 
-        return self.first_name + self.last_name
+        return self.name
 
     def get_short_name(self):
         """Use to get a user's short nmae."""
 
-        return self.first_name
+        return self.name
 
     def __str__(self):
         """Convert a user object to a string."""
